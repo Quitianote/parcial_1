05e4433434122fc22d1a7c534401c46d7f93917e
Quitianote <santiago.quitian@udea.edu.co>
2023-04-17 21:12:39 -0500

    antes de empezar con lo de leer archivo de usuario

--- parcial_1/parcial_1/main.cpp
+++ parcial_1/parcial_1/main.cpp
@@ -3,24 +3,39 @@
 
 using namespace std;
 
-void leermaterias(char reng[], char mat[][20]);
+bool leermaterias(char reng[], char mat[][20], char codi[], int co[]);
 
 int main(){
     ifstream texto;
     ifstream usuario;
     char reng[256];
-    char temp;
+    char codi[7];
     char usu[60];
-    char fisica[14][20] = {""};
+    char materia[14][20] = {""};
     char ingles[14][20] = {""};
     char algebra[14][20] = {""};
     char calculo[14][20] = {""};
     char infor[14][20] = {""};
     char intro[14][20] = {""};
     int ini = 0;
+    int contmat[2];
+    bool aux = true;
 
     texto.open("materias.txt");
+
+    cout << "Ingrese codigo de materia: ";cin >> codi;
+
+    while(texto.good()){
+        texto.getline(reng, 256);
+        aux = leermaterias(reng, materia, codi, contmat);
+        if(aux){
+            cout << "se encontro la materia" << endl;
+            break;
+        }
+    }
+    if(!aux) cout << "no se encontro la materia" << endl;
     //leyendo renglon
+    /*
     texto.getline(reng, 256);
     leermaterias(reng, fisica);
     texto.getline(reng, 256);
@@ -33,18 +48,14 @@ int main(){
     leermaterias(reng, infor);
     texto.getline(reng, 256);
     leermaterias(reng, intro);
-
+    */
     //tamaño de los arreglos de las materias
-    int tamfisica = 8;
-    int tamingles = 8;
-    int tamalgebra = 8;
-    int tamcalculo = 8;
-    int taminfor = 12;
-    int tamintro = 5;
 
-    for(int i = 0; i < 8; i ++){
-        cout << fisica[i] << endl;
+    for(int i = 0; i < 14; i ++){
+        cout << materia[i] << endl;
     }
+    cout << "contador: " << contmat[0] << endl;
+    /*
     for(int i = 0; i < 8; i ++){
         cout << ingles[i] << endl;
     }
@@ -59,15 +70,17 @@ int main(){
     }
     for(int i = 0; i < 5; i ++){
         cout << intro[i] << endl;
-    }
+    }*/
 
     while(true){
-        cout << "Ingrese: " << endl << "'1' Para acceder a un horario." << endl << "'0' Para salir del programa." << endl; cin >> ini;//mirando si cierra el programa o quiere abrir un horario
-        while(ini != 0 && ini != 1){
+        cout << "*****MENU*****" << endl;
+        cout << endl;
+        cout << "Ingrese: " << endl << "'1' Para acceder a un horario." << endl << "'0' Para salir del programa." << endl << "'2' Para crear un horario." << endl; cin >> ini;//mirando si cierra el programa o quiere abrir un horario
+        while(ini != 0 && ini != 1 && ini != 2){
             cout << "Error::Numero invalido." << endl;cin >> ini;
         }
-        if(ini == 0) break;
-        else{//quiso mirar un archivo
+        if(ini == 0) break;//quiso salir del programa
+        else if(ini == 1){//quiso mirar un archivo
             cout << "Ingrese nombre del archivo del usuario: ";cin >> usu;
             usuario.open(usu);
 
@@ -81,10 +94,9 @@ int main(){
     return 0;
 }
 
-void leermaterias(char reng[], char mat[14][20]){//leer renglon de materias
-    char pala[20];
+bool leermaterias(char reng[], char mat[14][20], char codi[], int co[]){//leer renglon de materias
     int pos = 0;
-    int cont = 2;
+    int cont = 4;
     bool aux = false;
 
     for(int i = 0; i < 256; i ++){
@@ -93,7 +105,11 @@ void leermaterias(char reng[], char mat[14][20]){//leer renglon de materias
                 if(reng[a] != '!'){
                     mat[0][pos] = reng[a];//posicion en la matriz
                     cout << mat[0] << " codigo" << endl;
+                    char temp1 = mat[0][pos];
+                    char temp2 = codi[pos];
+                    if(temp1 != temp2) return false;
                     pos ++;
+
                 }
                 else break;
             }
@@ -119,10 +135,22 @@ void leermaterias(char reng[], char mat[14][20]){//leer renglon de materias
         else if(reng[i] == '#'){
             pos = 0;
             for(int a = i + 1; a < 256; a ++){//creando numero de horas docente
-                if(reng[a] != '/'){
+                if(reng[a] != '?'){
                     mat[2][pos] = reng[a];
                     cout << a << " a" << endl;
                     cout << mat[2] << " numero" << endl;
+                    pos ++;//posicion en matriz
+                }
+                else break;
+            }
+        }
+        else if(reng[i] == '?'){
+            pos = 0;
+            for(int a = i + 1; a < 256; a ++){//creando creditos
+                if(reng[a] != '/'){
+                    mat[3][pos] = reng[a];
+                    cout << a << " a" << endl;
+                    cout << mat[3] << " numero" << endl;
                     pos ++;//posicion en matriz
                 }
                 else break;
@@ -153,14 +181,16 @@ void leermaterias(char reng[], char mat[14][20]){//leer renglon de materias
                     cout << cont << " posicion" << endl;
                 }
                 else if(reng[a] == '-'){//final de materia
+                    co[0] = cont + 1;
                     aux = true;
                     break;
                 }
             }
         }
-        if(aux) break;
+        if(aux) break;//verificando si
         cout << i << " i" << endl;
     }
+    return true;
 }
 
 
