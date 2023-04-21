cccc70fb9684bd0bdce2b47ade5c8b46d8916b03
Quitianote <santiago.quitian@udea.edu.co>
2023-04-20 23:27:20 -0500

    falta modificar arreglo del dia para luego escribirlo en el archivo

--- parcial_1/parcial_1/main.cpp
+++ parcial_1/parcial_1/main.cpp
@@ -3,15 +3,19 @@
 
 using namespace std;
 
-bool leermaterias(char reng[], char mat[][20], char codi[], int co[]);
+bool leermaterias(char reng[], char mat[][20], char codi[]);
 bool verificar_num(char codi[]);
 bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][9], char V[][9], char S[][9], char D[][9]);
 void leerusu(char reng[], char dia[][9]);
-
+void crear_mat(char mat[16][20], char codi[], char nom[], char hor[], char credi[]);
+void modificar_codi(char dia[16][9], int hora, char codi[]);
+bool verificar_libre(int hora, char codi[]);
+void vaciar(char a[16][9], int pos);
 
 int main(){
     ifstream texto;
     ifstream usuario;
+    ofstream lect_mat;
     char L[16][9] = {""};
     char M[16][9] = {""};
     char X[16][9] = {""};
@@ -19,11 +23,18 @@ int main(){
     char V[16][9] = {""};
     char S[16][9] = {""};
     char D[16][9] = {""};
+    char dia = '\0';
+    char temp;
     char reng[256];
     char rengd[256];
-    char codi[7];
+    char codi[8];
+    char nom_mat[17] = "";
+    char num_do[2] = "";
+    char creditos[2] = "";
     char usu[60] = "";
     char materia[14][20] = {""};
+    char mat_new[16][20] = {""};
+    char mat_tot[256] = "";
     /*
     char fisica[14][20] = {""};
     char ingles[14][20] = {""};
@@ -31,14 +42,17 @@ int main(){
     char calculo[14][20] = {""};
     char infor[14][20] = {""};
     char intro[14][20] = {""};*/
+    int h = 0;
+    int tam = 0;
     int modi = 0;
     int ini = 0;
+    int deci = 0;
     int contmat[2];
     bool aux = true;
     bool aux_exis = true;
 
     texto.open("materias.txt");
-
+/*
     cout << "Ingrese codigo de materia: ";cin >> codi;
 
     while(texto.good()){
@@ -49,70 +63,8 @@ int main(){
             break;
         }
     }
-    if(!aux) cout << "no se encontro la materia" << endl;
-
-
-    //leyendo archivo usuario prueba
-
-
-    /*
-    texto.getline(reng, 256);
-    leermaterias(reng, fisica, codi, contmat);
-    texto.getline(reng, 256);
-    leermaterias(reng, ingles, codi, contmat);
-    texto.getline(reng, 256);
-    leermaterias(reng, algebra, codi, contmat);
-    texto.getline(reng, 256);
-    leermaterias(reng, calculo, codi, contmat);
-    texto.getline(reng, 256);
-    leermaterias(reng, infor, codi, contmat);
-    texto.getline(reng, 256);
-    leermaterias(reng, intro, codi, contmat);
-    */
+    if(!aux) cout << "no se encontro la materia" << endl;*/
 /*
-    usuario.open("jaime.txt");
-    cout << "is open: " << usuario.is_open() << endl;
-    cout << "archivo: " << usu << endl;
-
-    cout << "Horario "<< usu << " abierto." << endl << endl << endl;*/
-/*
-    usuario.getline(rengd, 256);//leyendo horario de usuario
-    leerusu(rengd, M);
-    for(int i = 0; i < 16; i ++){//forma que encontre para solucionar el problema de segmentacion de memoria
-        for(int a = 0; a < 9; a ++){
-            L[i][a] = M[i][a];
-        }
-    }
-    usuario.getline(rengd, 256);
-    leerusu(rengd, M);
-    usuario.getline(rengd, 256);
-    leerusu(rengd, X);
-    usuario.getline(rengd, 256);
-    leerusu(rengd, J);
-    usuario.getline(rengd, 256);
-    leerusu(rengd, V);
-    usuario.getline(rengd, 256);
-    leerusu(rengd, S);
-    usuario.getline(rengd, 256);
-    leerusu(rengd, D);
-    //imprimir horario
-    int i = 0;
-    int a = 0;
-    int hora = 7;//hora
-    //cout << "Dia" << endl; //-----6-----7-----8-----9-----10-----11-----12-----13-----14-----15-----16-----17-----18-----19-----20-----21-----22-----" << endl;
-    cout << "Hora ----- Lunes -------- Martes --------- Miercoles ----------- Jueves ---------------- Viernes ------------- Sabado --------------- Domingo ---|" << endl;
-
-    for(; i < 16; i ++){
-        if(hora < 10) cout << "0" << hora << ":  ---| " << L[i] << "  ----| " << M[i] << "  ------| " << X[i] << "  --------| " << J[i] << "  -------------| " << V[i] << "  --------| " << S[i] << "  --------------| " << D[i] << "  |" << endl;
-        else cout << hora << ":  ---| " << L[i] << "  ----| " << M[i] << "  ------| " << X[i] << "  --------| " << J[i] << "  -------------| " << V[i] << "  --------| " << S[i] << "  --------------| " << D[i] << "  |" << endl;
-        hora ++;
-    }
-*/
-
-
-
-
-    //tamaño de los arreglos de las materias
 
     for(int i = 0; i < 14; i ++){
         cout << materia[i] << endl;
@@ -125,8 +77,8 @@ int main(){
         cout << V[i] << " :dia 5" << endl;
         cout << S[i] << " :dia 6" << endl;
         cout << D[i] << " :dia 7" << endl;
-    }
-    cout << "contador: " << contmat[0] << endl;
+    }*/
+    //cout << "contador: " << contmat[0] << endl;
     /*
     for(int i = 0; i < 8; i ++){
         cout << ingles[i] << endl;
@@ -156,7 +108,7 @@ int main(){
             for(;;){
                 cout << endl;
                 cout << "Ingrese nombre del archivo del usuario o '0' si desea volver: ";cin >> usu;
-                //usuario.open(usu);
+
                 if(usu[0] == '0') break;
                 usuario.close();
                 usuario.open(usu);
@@ -239,19 +191,67 @@ int main(){
                                 texto.seekg(0);
                                 while(texto.good()){//verificando si el codigo esta en la base de datos
                                     texto.getline(reng, 256);
-                                    aux = leermaterias(reng, materia, codi, contmat);
-                                    cout << "renglon: " << reng << endl;
+                                    aux = leermaterias(reng, materia, codi);
+
                                     if(aux){
-                                        cout << "se encontro la materia" << endl;
                                         break;
                                     }
                                 }
-                                if(!aux){
-                                    cout << "no se encontro la materia" << endl;
+
+                                if(!aux){//sino se encontro el codigo en la base de datos se le preguntara al usuario si quiere agregar el codigo como una nueva materia aen la base de datos
+                                    cout << "No se encontro el codigo, Desea agregar la materia a la base de datos?" << endl << "Ingrese: " << endl  << "'0' Para no." << endl << "'1' Para si." << endl;cin >> deci;
+                                    while(deci != 0 && deci != 1){
+                                        cout << "Error::Numero invalido." << endl;cin >> deci;
+                                    }
+                                    if(deci == 0) continue;
+                                    else{
+                                        cout << "Ingrese nombre de materia(maximo 16 caracteres y con '_' en lugar de espacios): ";cin >> nom_mat;
+                                        cout << "Ingrese numero de horas de docente a la semana: ";cin >> num_do;
+                                        cout << "Ingrese creditos: ";cin >> creditos;
+                                        crear_mat(mat_new, codi, nom_mat, num_do, creditos);
+                                        texto.seekg(0);
+                                        tam = 1;
+                                        for(i = 0; texto.good();i ++){//guardando todo el archivo materia en un arreglo
+                                            temp = texto.get();
+                                            if(temp != EOF){//que sea diferente al -1 que retorna al leer caracter por caracter, se le llama EOF
+                                                mat_tot[i] = temp;
+                                                tam ++;
+                                            }
+                                        }
+                                        cout << "codi: " << codi << endl;
+                                        texto.close();//cerrando archivo en modo lectura
+                                        lect_mat.open("materias.txt");//abriendo archivo en modo escritura
+                                        for(i = 0; i < tam; i ++){//escribiendo materias normales
+                                            lect_mat.put(mat_tot[i]);
+                                            cout << "escri 1: " << mat_tot[i] << endl;
+                                        }
+                                        lect_mat.put('\n');
+                                        for(i = 0; i < 16; i ++){//escribiendo materia nueva
+                                            for(int a = 0; a < 20; a ++){
+                                                if(mat_new[i][a] == '\0') break;
+                                                else{
+                                                    lect_mat.put(mat_new[i][a]);
+                                                    cout << "escri 2: " << mat_new[i][a] << endl;
+                                                }
+                                            }
+                                            if(i == 0) lect_mat.put('!');
+                                            else if(i == 1) lect_mat.put('#');
+                                            else if(i == 2) lect_mat.put('?');
+                                            else if(i == 3){
+                                                lect_mat.put('-');
+                                                break;
+                                            }
+                                        }
+                                        lect_mat.close();//cerrando archivo modo escritura
+                                        texto.open("materias.txt");//abriendo archivo modo lectura
+                                    }
+                                }
+                                else{//el archivo si esta en la base de datos, por lo tanto le pregunta al usuario el horario de su materia
+                                    //como ya anteriormente use la funcion para buscar la materia con el codigo, entonces ya se abra guardado en el arreglo materia
+
+
 
                                 }
-
-
                             }
                         }
                     }
@@ -272,7 +272,7 @@ int main(){
     return 0;
 }
 
-bool leermaterias(char reng[], char mat[14][20], char codi[], int co[]){//leer renglon de materias
+bool leermaterias(char reng[], char mat[14][20], char codi[]){//leer renglon de materias
     int pos = 0;
     int cont = 4;
     bool aux = false;
@@ -334,41 +334,9 @@ bool leermaterias(char reng[], char mat[14][20], char codi[], int co[]){//leer r
                 else break;
             }
         }
-        else if(reng[i] == '/'){
-            pos = 0;
-            for(int a = i + 1; a < 256; a ++){//creando horario
-                if(reng[a] - 0 < 90 && reng[a] - 0 > 65){//mirando si es mayuscula
-                    pos = 0;
-                    mat[cont][pos] = reng[a];//guardando dia
-                   // cout << mat[3] << " dia 1" << endl;
-                    //cout << mat[3] << " dia 2" << endl;
-                    //cout << "letra de dia: " << reng[a] << endl;
-                }
-                else if(reng[a] != ',' && reng[a] != '/' && reng[a] != '-'){//guardando hora
-                    mat[cont][pos] = reng[a];
-                    //cout << reng[a] << " letra" << endl;
-                    pos ++;
-                }
-                if(reng[a] == ',' || reng[a] == '/'){//siguiente hora o dia
-                    cont ++;
-                    pos = 0;
-                    /*
-                    cout << reng[a] << " letra" << endl;
-                    cout << mat[4] << " hora 3" << endl;
-                    cout << mat[5] << " hora 4" << endl;
-                    cout << mat[6] << " hora 5" << endl;
-                    cout << cont << " posicion" << endl;
-                    */
-                }
-                else if(reng[a] == '-'){//final de materia
-                    co[0] = cont + 1;
-                    aux = true;
-                    break;
-                }
-            }
+        else if(reng[i] == '-'){//final de materia
+            break;
         }
-        if(aux) break;//verificando si ya finalizo el renglon
-        //cout << i << " i" << endl;
     }
     return true;
 }
@@ -442,7 +410,6 @@ bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][
                 temp[a] = M[i][(a + 1)];//construyendo arreglo que luego se va a comparar
             }
             if(temp[0] == codi[0] && temp[1] == codi[1] && temp[2] == codi[2] && temp[3] == codi[3] && temp[4] == codi[4] && temp[5] == codi[5] && temp[6] == codi[6])return true;
-           a = 0;
         }
     }
 
@@ -452,7 +419,6 @@ bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][
                 temp[a] = X[i][(a + 1)];//construyendo arreglo que luego se va a comparar
             }
             if(temp[0] == codi[0] && temp[1] == codi[1] && temp[2] == codi[2] && temp[3] == codi[3] && temp[4] == codi[4] && temp[5] == codi[5] && temp[6] == codi[6])return true;
-            a = 0;
         }
     }
 
@@ -461,7 +427,6 @@ bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][
             for(a = 0; a < 9; a ++){
                 temp[a] = J[i][(a + 1)];//construyendo arreglo que luego se va a comparar
             }
-            a = 0;
             if(temp[0] == codi[0] && temp[1] == codi[1] && temp[2] == codi[2] && temp[3] == codi[3] && temp[4] == codi[4] && temp[5] == codi[5] && temp[6] == codi[6])return true;
          }
     }
@@ -472,7 +437,6 @@ bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][
                 temp[a] = V[i][(a + 1)];//construyendo arreglo que luego se va a comparar
             }
             if(temp[0] == codi[0] && temp[1] == codi[1] && temp[2] == codi[2] && temp[3] == codi[3] && temp[4] == codi[4] && temp[5] == codi[5] && temp[6] == codi[6])return true;
-            a = 0;
          }
     }
 
@@ -482,7 +446,6 @@ bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][
                 temp[a] = S[i][(a + 1)];//construyendo arreglo que luego se va a comparar
             }
             if(temp[0] == codi[0] && temp[1] == codi[1] && temp[2] == codi[2] && temp[3] == codi[3] && temp[4] == codi[4] && temp[5] == codi[5] && temp[6] == codi[6])return true;
-            a = 0;
          }
     }
 
@@ -492,10 +455,94 @@ bool verificar_exis(char codi[], char L[][9],char M[][9], char X[][9], char J[][
                 temp[a] = D[i][(a + 1)];//construyendo arreglo que luego se va a comparar
             }
             if(temp[0] == codi[0] && temp[1] == codi[1] && temp[2] == codi[2] && temp[3] == codi[3] && temp[4] == codi[4] && temp[5] == codi[5] && temp[6] == codi[6])return true;
-            a = 0;
          }
     }
 
     return false;
 }
+
+void crear_mat(char mat[16][20], char codi[], char nom[], char hor[], char credi[]){//creando materia para registrar en la base de datos
+    int pos = 0;
+    mat[0][0] = '*';
+
+    for(int i = 1; i < 9; i ++){//guardando codigo
+        if(i == 8) mat[0][8] = '\0';
+        else{
+            mat[0][i] = codi[pos];
+            pos ++;
+            cout << "mat codi: " << mat[0] << endl;
+        }
+    }
+    for(int i = 0; i < 17; i ++){//guardando nombre de materia
+       mat[1][i] = nom[i];
+       cout << "mat nom: " << mat[1] << endl;
+    }
+    mat[2][0] = hor[0];//guardando horas de docente
+    mat[2][1] = '\0';
+    cout << "mat hor: " << mat[2] << endl;
+    mat[3][0] = credi[0];//guardando creditos
+    mat[3][1] = '\0';
+    cout << "mat credi: " << mat[3] << endl;
+}
+
+bool verificar_libre(int hora, char dia[16][9]){//verificar si la posicion esta libre para modificarla
+    char temp[8] = "";
+    hora = hora - 7;//obteniendo posicion
+
+    for(int i = 0; i < 8; i ++){//creando arreglo temporal que se va a comparar
+        if(dia[hora][i] != ' '){
+            temp[i] = dia[hora][i];
+        }
+        else break;
+    }
+
+    if(temp == "Libre") return true;
+    return false;
+}
+
+void modificar(char L[16][9], char M[16][9], char X[16][9], char J[16][9], char V[16][9], char S[16][9], char D[16][9], char codi[]){//pidiendole al usuario los dias a modificar
+    bool aux = true;
+    char dia = '\0';
+    int h = 0;
+
+    for(;;){
+        cout << endl << "Ingrese el dia que ve la materia(en mayuscula): ";cin >> dia;
+        switch(dia){
+            case 'L':
+            for(;;){
+                cout << endl << "Ingrese hora a la que ve la materia(en formato de 24 horas): ";cin >> h;
+                aux = verificar_libre(h, L);
+                while(!aux){
+                    cout << endl << "Error::No se puede agregar la hora porque ya esta ocupada" << endl << "Vuelva a intentar: ";cin >> h;
+                    aux = verificar_libre(h, L);
+                }
+                vaciar(L, h);
+                modificar_codi(L, h, codi);
+                cout << "materia nueva: " << L[h-7] << endl;
+
+            }
+            break;
+        }
+
+    }
+}
+
+void modificar_codi(char dia[16][9], int hora, char codi[]){
+    int pos = 0;
+    hora = hora - 7;
+    dia[hora][0] = '*';
+
+    for(int i = 1; i < 9; i ++){//vaciando posicion del arreglo
+        dia[hora][i] = codi[pos];
+        pos ++;
+    }
+}
+
+void vaciar(char a[16][9], int pos){//vaciando la posicion que se va reemplazar por la materia
+    pos = pos - 7;//obteniendo posicion
+    for(int i = 0; i < 9; i ++){//vaciando posicion del arreglo
+        a[pos][i] = '\0';
+    }
+}
+
 
